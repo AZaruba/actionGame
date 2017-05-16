4404906900d96c6f05d871b1abf6d64f966f2d8a
Andrew Zaruba <A.B.Zaruba@gmail.com>
2017-05-16 16:26:36 -0500

    Initial Commit, camera follows game object currently.

---
+++ Assets/Scripts/Camera/CameraScript.cs
@@ -0,0 +1,24 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class CameraScript : MonoBehaviour
+{
+
+    public RenderTexture renderTexture;
+
+    void Start()
+    {
+
+    }
+
+    void OnGUI()
+    { 
+        GUI.depth = 20;
+        GUI.DrawTexture(new Rect(0, 0, Screen.width, Screen.height), renderTexture);
+    }
+
+    void Update()
+    {
+        transform.LookAt(GameObject.Find("Player").transform);
+    }    
+}
---
+++ Assets/Scripts/Player/PlayerMovement.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerMovement : MonoBehaviour {
+
+    public float walkSpeed;
+    private Camera mainCam;
+
+	// Use this for initialization
+	void Start () {
+
+    }
+
+    Vector3 walkInput()
+    {
+        mainCam = Camera.main;
+        Vector3 direction = new Vector3();
+        Vector3 cameraDirection = new Vector3(mainCam.transform.forward.x, 0, mainCam.transform.forward.z);
+        direction.z = Input.GetAxis("Vertical");
+        direction.x = Input.GetAxis("Horizontal");
+
+        direction.Normalize();
+        cameraDirection.Normalize();
+
+        direction *= walkSpeed * Time.deltaTime;
+        return direction;
+    }
+	
+	// Update is called once per frame
+	void Update () {
+        
+        transform.position += walkInput();
+    }
+}
