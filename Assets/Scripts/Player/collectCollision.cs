c8ade7275d3b508ba623639fdaea25b0350134fe
Andrew Zaruba <A.B.Zaruba@gmail.com>
2017-05-25 19:26:35 -0500

    collision detection with coins now works

---
+++ Assets/Scripts/Player/collectCollision.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class collectCollision : MonoBehaviour {
+
+	private RaycastHit coinHit;
+	private Collider[] hitArray;
+	private int layerMask;
+
+	// Use this for initialization
+	void Start () {
+		layerMask = 1 << 8;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		hitArray = Physics.OverlapSphere (transform.position, 1.0f, layerMask);
+		if (hitArray.Length > 0) {
+			Destroy (hitArray [0].gameObject);
+		}
+	}
+}
