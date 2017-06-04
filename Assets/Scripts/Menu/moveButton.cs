b75148657aa996dd05ad37806f6bc9feccc49ece
Andrew Zaruba <A.B.Zaruba@gmail.com>
2017-06-04 13:20:44 -0500

    started experimenting with menus! Mostly navigation instead of functionality

---
+++ Assets/Scripts/Menu/moveButton.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class moveButton : MonoBehaviour {
+
+	public float horizPosition;
+	public float[] vertPositions;
+
+	private int index;
+	private RectTransform thisTransform;
+
+	// Use this for initialization
+	void Start () {
+		thisTransform = gameObject.GetComponent<RectTransform> ();
+		thisTransform.anchoredPosition = new Vector2 (horizPosition, vertPositions [0]);
+		index = 0;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		if (Input.GetAxis ("Vertical") < 0 && index != (vertPositions.Length - 1)) {
+			index++;
+			thisTransform.anchoredPosition = new Vector2 (horizPosition, vertPositions [index]);
+		}
+		if (Input.GetAxis ("Vertical") > 0 && index != 0) {
+			index--;
+			thisTransform.anchoredPosition = new Vector2 (horizPosition, vertPositions [index]);
+		}
+	}
+}
