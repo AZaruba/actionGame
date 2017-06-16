5bc8ee51e5faa89f16e7121f61556e763e6f8ee6
Andrew Zaruba <A.B.Zaruba@gmail.com>
2017-06-16 14:34:39 -0500

    level transitions are in place

---
+++ Assets/GUI/moveToNextLevel.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class moveToNextLevel : MonoBehaviour {
+
+	public string levelName;
+
+	// Update is called once per frame
+	void Update () {
+		if (!GameObject.Find ("blueCrystal")) {
+			if (Input.GetKeyDown (KeyCode.O)) {
+				SceneManager.LoadScene (levelName, LoadSceneMode.Single);
+			}
+		}
+			
+	}
+}
